944bd65a7c3394920e7d6d31e9f888346ae04f2c
Ibidapo <dapsined@gmail.com>
2018-11-09 11:31:11 +0100

    endpoints created and test integrated

--- parcels/find-parcel.js
+++
@@ -1,8 +0,0 @@
-let parcelModule = require('./parcel');
-let parcels = parcelModule.parcels;
-
-const findParcel = (reqParam) => parcels.filter((parcel) =>  parcel.parcelId == reqParam);
-
-const findParcelByUser = (reqParam) => parcels.filter((parcel) => parcel.userId == reqParam);
-
-module.exports = { findParcel, findParcelByUser };
